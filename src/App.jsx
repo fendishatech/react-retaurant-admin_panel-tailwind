758f11fd6a8dea574d58b98119143a17efda7635
fendishatech <liyukibir@gmail.com>
2023-06-15 04:59:33 -0700

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="m-6 border-[2px] border-indigo-600 rounded">
+      <h1 className="py-[3rem] text-center text-5xl font-bold text-indigo-600">
+        Vite + React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
